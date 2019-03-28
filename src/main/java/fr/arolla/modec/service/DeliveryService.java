27982f91cf4b6951b722573d58dca53ce544d6a9
Arnauld <arnauld.loyer@gmail.com>
2019-03-28 23:15:51 +0100

    move transactional to service, in absence of usecase/controller services have to handle the transaction management + hack for lazy issue while traversing list on cart

--- src/main/java/fr/arolla/modec/service/DeliveryService.java
+++ src/main/java/fr/arolla/modec/service/DeliveryService.java
@@ -3,8 +3,12 @@ package fr.arolla.modec.service;
 import fr.arolla.modec.entity.DeliveryId;
 import fr.arolla.modec.entity.Order;
 import fr.arolla.modec.entity.OrderId;
+import fr.arolla.modec.exception.OrderNotFoundException;
 import fr.arolla.modec.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class DeliveryService {
 
     private final OrderRepository orderRepository;
@@ -13,8 +17,9 @@ public class DeliveryService {
         this.orderRepository = orderRepository;
     }
 
+    @Transactional
     public DeliveryId createDeliveryFromOrder(OrderId orderId) {
-        orderRepository.findById(orderId).get().setStatus(Order.Status.IN_PREPARATION);
+        orderRepository.findById(orderId).orElseThrow(() -> new OrderNotFoundException(orderId)).setStatus(Order.Status.IN_PREPARATION);
         return new DeliveryId(orderId.getId());
     }
 }
