c1533d8c8c56a36325efba37493ed46cb776c54e
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2019-03-29 22:18:58 +0100

    Creating directory structure

--- src/main/java/fr/arolla/modec/repository/CartLineRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.arolla.modec.repository;
-
-import fr.arolla.modec.entity.CartLine;
-
-public interface CartLineRepository {
-    CartLine save(CartLine cartLine);
-}
--- src/main/java/fr/arolla/modec/repository/OrderLineRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.arolla.modec.repository;
-
-import fr.arolla.modec.entity.OrderLine;
-
-public interface OrderLineRepository {
-    OrderLine save(OrderLine orderLine);
-}
